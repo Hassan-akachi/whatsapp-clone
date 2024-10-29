37d215cdbac1af232b0dfff6839a2efb40ca3227
hassan <hassansorunke@gmail.com>
2024-10-29 23:30:45 +0100

    update send audio working fine

--- lib/features/chat/widgets/bottom_chat_field.dart
+++ lib/features/chat/widgets/bottom_chat_field.dart
@@ -3,6 +3,9 @@ import 'dart:io';
 import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_sound/flutter_sound.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:whatsapp_ui/common/enums/message_enums.dart';
 import 'package:whatsapp_ui/features/chat/controller/chat_controller.dart';
 
@@ -25,7 +28,17 @@ class _BottomChatFieldState extends ConsumerState<BottomChatField> {
   final TextEditingController _messageController = TextEditingController();
   bool isShowSendButton = false; // toggle btw send and audio rec
   bool isShowEmojiContainer = false;
-  FocusNode focusNode = FocusNode();
+  FocusNode focusNode = FocusNode(); // to control keyboard
+  FlutterSoundRecorder? _soundRecorder;
+  bool isRecorderInit = false;
+  bool isRecording = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _soundRecorder = FlutterSoundRecorder(); //initialized here
+    openAudio();
+  }
 
   void sendTextMessage() async {
     if (isShowSendButton) {
@@ -33,6 +46,22 @@ class _BottomChatFieldState extends ConsumerState<BottomChatField> {
           context, _messageController.text.trim(), widget.receiverUserId);
       setState(() {
         _messageController.text = "";
+      });
+    } else {
+      //send audio recording
+      var tempDir = await getTemporaryDirectory();
+      var path = '${tempDir.path}/flutter_sound.aac';
+      if (!isRecorderInit) {
+        return;
+      } //don't proceed
+      if (isRecording) {
+        await _soundRecorder!.stopRecorder();
+        selectFileMessage(File(path), MessageEnum.audio);
+      } else {
+        await _soundRecorder!.startRecorder(toFile: path);
+      }
+      setState(() {
+        isRecording = !isRecording;
       });
     }
   }
@@ -73,6 +102,16 @@ class _BottomChatFieldState extends ConsumerState<BottomChatField> {
   void toggleEmojiKeyboardContainer() {
     toggleKeyboard();
     toggleEmojiContainer();
+  }
+
+  void openAudio() async {
+    //access permission to open microphone
+    final status = await Permission.microphone.request();
+    if (status != PermissionStatus.granted) {
+      throw Exception('Mic permission not allowed!');
+    }
+    await _soundRecorder!.openRecorder();
+    isRecorderInit = true;
   }
 
   @override
@@ -165,7 +204,12 @@ class _BottomChatFieldState extends ConsumerState<BottomChatField> {
                       backgroundColor: whatsappGreen,
                       radius: 25,
                       child: GestureDetector(
-                        child: Icon(isShowSendButton ? Icons.send : Icons.mic,
+                        child: Icon(
+                            isShowSendButton
+                                ? Icons.send
+                                : isRecording
+                                    ? Icons.stop
+                                    : Icons.mic,
                             color: Colors.white),
                         onTap: sendTextMessage,
                       ))),
@@ -181,9 +225,9 @@ class _BottomChatFieldState extends ConsumerState<BottomChatField> {
                     setState(() {
                       _messageController.text += emoji.emoji;
                     });
-                    if(!isShowSendButton){
+                    if (!isShowSendButton) {
                       setState(() {
-                        isShowSendButton =true;
+                        isShowSendButton = true;
                       });
                     }
                   },
@@ -198,5 +242,7 @@ class _BottomChatFieldState extends ConsumerState<BottomChatField> {
   void dispose() {
     super.dispose();
     _messageController.dispose();
+    _soundRecorder!.closeRecorder();
+    isRecorderInit = false;
   }
 }
--- lib/features/chat/widgets/display_text_files.dart
+++ lib/features/chat/widgets/display_text_files.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp_ui/common/enums/message_enums.dart';
@@ -12,13 +13,37 @@ class DisplayTextImagesGif extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bool isPlaying = false;
+    final AudioPlayer audioPlayer = AudioPlayer();
     return messageType == MessageEnum.text
         ? Text(
             message,
             style: const TextStyle(fontSize: 16),
           )
         : messageType == MessageEnum.image
-            ? CachedNetworkImage(imageUrl: message) // this a dependency that help cache the to avoid multiple recall
-            : VideoPlayerItem(videoUrl: message);
+            ? CachedNetworkImage(
+                imageUrl:
+                    message) // this a dependency that help cache the to avoid multiple recall
+            : messageType == MessageEnum.video
+                ? VideoPlayerItem(videoUrl: message)
+                : StatefulBuilder(builder: (context, setState) {
+                    return IconButton(
+                        constraints: const BoxConstraints(minWidth: 120),
+                        onPressed: () async {
+                          if (isPlaying) {//if audio is playing stop
+                            await audioPlayer.pause();
+                            setState(() {
+                              isPlaying = false;
+                            });
+                          } else {
+                            audioPlayer.play(UrlSource(message));
+                            setState(() {
+                              isPlaying = true;
+                            });
+                          }
+                        },
+                        icon: isPlaying ? const Icon(Icons.play_circle)
+                            :const Icon(Icons.stop_circle));
+                  });
   }
 }
